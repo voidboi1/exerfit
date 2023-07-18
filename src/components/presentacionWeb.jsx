6da39e8d236768fd19be2513044c4d35e05a4517
Nicolas Alvarado <139598317+voidboi1@users.noreply.github.com>
2023-07-18 16:31:22 -0400

    ..

--- src/components/presentacionWeb.jsx
+++ src/components/presentacionWeb.jsx
@@ -12,11 +12,8 @@ export const Web = () => {
 
                     <div className="col-lg-6">
                         <h1 className="display-5 fw-bold text-white lh-1 mb-3">ExerFit</h1>
-                        <p className="lead text-white">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-                        <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-                            <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-                            <button type="button" className="btn btn-outline-secondary btn-lg px-4">Default</button>
-                        </div>
+                        <p className="lead text-white">Exerfit es una página dónde podrás encontrar diversas rutinas para entrenar en casa, también podrás seguir tus entrenamientos y guardar tus rutinas favoritas.</p>
+                      
                     </div>
                 </div>
             </div>
